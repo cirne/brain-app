b4cd3c95ccf693b844e24783f0fb17c63f80a146
Lewis Cirne <lewiscirne@mac.com>
2026-04-16 15:36:21 -0500

    auto-sync: 2026-04-16 20:36

--- src/server/lib/syncAll.ts
+++ src/server/lib/syncAll.ts
@@ -3,6 +3,7 @@ import { promisify } from 'node:util'
 import { parseICS, writeCache } from './calendarCache.js'
 import { repoDir } from './wikiDir.js'
 import { formatExecError } from './execError.js'
+import { ripmailBin } from './ripmailBin.js'
 
 const execAsync = promisify(exec)
 
